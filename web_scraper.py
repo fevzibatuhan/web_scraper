05492b4f920d55095060ca475bcef50c629bb782
fevzibatuhan <121447621+fevzibatuhan@users.noreply.github.com>
2023-07-24 18:22:51 +0300

    Update web_scraper.py
    This is web scraper program. Used to get a view of the website.

--- web_scraper.py
+++ web_scraper.py
@@ -1,9 +1,7 @@
 import requests
-
 from bs4 import BeautifulSoup
 
-
-url = 'https://example.com' # Veri toplanacak web sayfasının URL'si
+url = input("Web sayfasını giriniz ('https://example.com'):") # Veri toplanacak web sayfasının URL'si
 
 response = requests.get(url) # Web sayfasını almak için GET isteği yapılır
 
@@ -11,5 +9,18 @@ soup = BeautifulSoup(response.text, 'html.parser') # Web sayfasının içeriği 
 
 basliklar = soup.find_all('h1') # Tüm başlıkları bul
 
+print("\n--- BAŞLIKLAR ---")
 for baslik in basliklar: # Her başlığı ekrana yazdır
     print(baslik.text)
+
+paragraflar = soup.find_all('p') # Tüm paragraf içeriklerini bul
+
+print("\n--- PARAGRAFLAR ---")
+for paragraf in paragraflar: # Her paragraf içeriğini ekrana yazdır
+    print(paragraf.text)
+
+linkler = soup.find_all('a') # Tüm bağlantıları (linkleri) bul
+
+print("\n--- BAĞLANTILAR ---")
+for link in linkler: # Her bağlantıyı ekrana yazdır
+    print(link.get('href'))
