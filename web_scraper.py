9f333d9dbd16f51c302fd42cbe4ee6a2240f8dc4
fevzibatuhan <121447621+fevzibatuhan@users.noreply.github.com>
2023-07-25 18:11:35 +0300

    Update web_scraper.py

--- web_scraper.py
+++ web_scraper.py
@@ -7,20 +7,25 @@ response = requests.get(url) # Web sayfasını almak için GET isteği yapılır
 
 soup = BeautifulSoup(response.text, 'html.parser') # Web sayfasının içeriği BeautifulSoup ile parse edilir
 
-basliklar = soup.find_all('h1') # Tüm başlıkları bul
+while True:
+    print("\n--- Başlıklar ---")
+    for baslik in basliklar: # Her başlığı ekrana yazdır
+        print(baslik.text)
 
-print("\n--- BAŞLIKLAR ---")
-for baslik in basliklar: # Her başlığı ekrana yazdır
-    print(baslik.text)
+    paragraflar = soup.find_all('p') # Tüm paragraf içeriklerini bul
 
-paragraflar = soup.find_all('p') # Tüm paragraf içeriklerini bul
+    print("\n--- Paragraflar ---")
+    for paragraf in paragraflar: # Her paragraf içeriğini ekrana yazdır
+        print(paragraf.text)
 
-print("\n--- PARAGRAFLAR ---")
-for paragraf in paragraflar: # Her paragraf içeriğini ekrana yazdır
-    print(paragraf.text)
+    linkler = soup.find_all('a') # Tüm bağlantıları (linkleri) bul
 
-linkler = soup.find_all('a') # Tüm bağlantıları (linkleri) bul
+    print("\n--- Bağlantılar ---")
+    for link in linkler: # Her bağlantıyı ekrana yazdır
+        print(link.get('href'))
 
-print("\n--- BAĞLANTILAR ---")
-for link in linkler: # Her bağlantıyı ekrana yazdır
-    print(link.get('href'))
+    cikis = input("Çıkış yapmak için 'x' tuşuna bas: ")
+    
+    if cikis == ord('q') or key == ord('x'):
+        break
+
